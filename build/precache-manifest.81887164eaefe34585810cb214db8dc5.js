5c2396e00ee7612f88ed68d8af02e85aa9aecbfc
chutiya <rishab1992.be20@chitkara.edu.in>
2023-09-20 21:04:05 +0530

    first commit

---
+++ build/precache-manifest.81887164eaefe34585810cb214db8dc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d6a553c3bb3823e77487a6c502e4bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9461031f3b2211730f5b",
+    "url": "/static/css/main.6adf1a87.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9461031f3b2211730f5b",
+    "url": "/static/js/main.575df396.chunk.js"
+  },
+  {
+    "revision": "30259e7a30d4e8b4ab16",
+    "url": "/static/js/runtime-main.a9559881.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/static/media/WeatherIcons.0a218296.gif"
+  }
+]);
