afa11eedff499caa4dc80725a5125a98e6e7007e
chutiya <rishab1992.be20@chitkara.edu.in>
2023-09-20 20:39:40 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0c9e428c4a05442a8e910d1f99e05829",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
